7cf7ce93481ee5046e2a9041af654a89e761f445
Shintaro <shintaro@ShintaronoiMac.local>
2020-01-26 22:25:34 +0900

    softmax import

---
+++ binary classification/softmax_test.py
@@ -0,0 +1,8 @@
+from sklearn.utils import shuffle
+from sklearn.datasets import fetch_mldata, fetch_openml
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+
+import numpy as np
+
+np.random.seed(34)
